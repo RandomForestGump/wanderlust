2c1f6115cf4d4a83152aa6b5b4bd79b28a8ed154
Rajat Jain <rajatjain.iitkgp@gmail.com>
2022-05-08 20:35:59 -0400

    Final demo

--- tour-contract/migrations/2_deploy_contracts.js
+++ tour-contract/migrations/2_deploy_contracts.js
@@ -1,14 +1,21 @@
 const tour = artifacts.require("tour");
 const token = artifacts.require("ERC20SHC");
 
-module.exports = function (deployer) {
-  deployer.deploy(token);
-  if (token.address) {
-    console.log(token.address);
-    deployer.deploy(tour, token.address);
-    // use deployedContract here
-}
-  
+// module.exports = function (deployer) {
+//   deployer.deploy(token);
+//   if (token.address) {
+//     console.log(token.address);
+//     deployer.deploy(tour, token.address);
+// }
+//     };
 
-  // deployer.deploy(token).then(function() {deployer.deploy(tour, token.deployed().address)});
-  };
+
+    module.exports = function (deployer) {
+      
+      deployer.then(async () => {
+        await deployer.deploy(token);
+        await deployer.deploy(tour, token.address);
+      });
+        };
+
+  
